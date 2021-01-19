72269224bf986559c272eeb008b35aa975ecaf40
fredericoeca <frederico-cabral@hotmail.com>
2021-01-19 17:20:33 -0300

    Prova tecnica Mobi7 finalizada

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -124,8 +124,16 @@ export class HomeComponent implements OnInit {
     let horas = periodo / (60 * 60 * 1000) - (dias * 24) >= 1 ? Math.floor(periodo / (60 * 60 * 1000) - (dias * 24)) : 0;
     let minutos = periodo / (60 * 1000) - (horas * 60 + (dias * 24 * 60)) >= 1 ? Math.floor(periodo / (60 * 1000) - (horas * 60 + (dias * 24 * 60))) : 0;
     let segundos = (periodo / 1000) - ((dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos * 60)) >= 1 ? Math.floor(periodo / 1000) - ((dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos * 60)) : 1;
-      
-    return `${dias} dia${ dias !== 1 ? `s` : `` }, ${horas} hora${ horas !== 1 ? `s` : `` }, ${minutos} minuto${ minutos !== 1 ? `s` : `` }, ${segundos} segundo${ segundos !== 1 ? `s` : `` }.`
+    
+    let diasStr = `${dias} dia${ dias !== 1 ? `s` : `` }`;
+    let horasStr = `${horas} hora${ horas !== 1 ? `s` : `` }`;
+    let minutosStr = `${minutos} minuto${ minutos !== 1 ? `s` : `` }`
+    let segundosStr = `${segundos} segundo${ segundos !== 1 ? `s` : `` }`
+    
+    return `${ dias !== 0 ? `${diasStr}, ` : `` }
+            ${ horas !== 0 ? `${horasStr}, ` : `` }
+            ${ minutos !== 0 ? `${minutosStr}, ` : `` }
+            ${ segundos !== 0 ? `${segundosStr}. ` : `` }`;     
   }
 
   filterMonitoramentoPorCarro(placa: string) {
